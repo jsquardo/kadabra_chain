559de31407d627e2c5130bd9bfa16aceeccefd58
Johnny Squardov <johnny@johnmichael.dev>
2021-04-23 15:24:09 -0400

    starting replace_chain function

--- backend/blockchain/blockchain.py
+++ backend/blockchain/blockchain.py
@@ -15,6 +15,23 @@ class Blockchain:
 
   def __repr__(self):
     return f'Blockchain: {self.chain}'
+  
+  def replace_chain(self, chain):
+    # Replace the local chain with the incoming one if the following applies:
+
+  @staticmethod
+  def is_valid_chain(chain):
+    # Validate the incoming chain
+    # Enforce following rules
+    # - chain must start with the genesis block
+    # - blocks must be formatted correctly
+    if chain[0] != Block.genesis():
+      raise Exception('The genesis block must be valid')
+
+    for i in range(1, len(chain)):
+      block = chain[i]
+      last_block = chain[i - 1]
+      Block.is_valid_block(last_block, block)
 
 
 def main():
